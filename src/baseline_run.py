9bbd9da31c7a2d03746f8b30358486f9f424d2ec
Jiwonjeon <jiwonjeon9603@kaist.ac.kr>
2025-09-02 19:39:36 +0900

    wandb jwjeon mscho

--- src/baseline_run.py
+++ src/baseline_run.py
@@ -71,7 +71,8 @@ def run(_run, _config, _log):
     wandb_name = f"agent={args.name}-mac={args.mac}-learner={args.learner}-mixer={args.mixer}-hier={detail}"
     _config["job"] = _config["name"]
     # _config = {k: str(v) for k, v in _config.items()}
-    wandb.login(relogin=True, key="ad42a1cee565925e2b5065efe7e76c329b954a29")
+    wandb.login(relogin=True, key="ad42a1cee565925e2b5065efe7e76c329b954a29") # jwjeon
+    # wandb.login(relogin=True, key="c65dcbd2cd1f30816b9a69b67cf462741ea48880") # mscho
     wandb.init(
         project="OffMTMARL",
         group=_config["task"],
